2cee66f20730d21041b3fa402f9f59427ae3b52b
Aleksey Gordeyev <agordeyev94@gmail.com>
2023-11-01 14:09:36 +0500

    initial commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,30 @@
+import Popover from './popover';
 
+const form = document.forms.buttonForPopover;
+
+const popoverFactory = new Popover();
+let actualMessages = [];
+
+const showPopover = (element) => {
+  actualMessages.push({
+    name: element.name,
+    id: popoverFactory.showPopover(element),
+  });
+};
+
+const hidePopover = (element) => {
+  const currentErrorMessage = actualMessages.find((item) => item.name === element.name);
+  if (currentErrorMessage) {
+    popoverFactory.hidePopover(currentErrorMessage.id);
+    actualMessages = actualMessages.filter((m) => m.id !== currentErrorMessage.id);
+  }
+};
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  if (actualMessages[0]) {
+    hidePopover(e.target.button);
+  } else {
+    showPopover(e.target.button);
+  }
+});
---
+++ src/js/popover.js
@@ -0,0 +1,44 @@
+export default class Popover {
+  constructor() {
+    this.popovers = [];
+
+    this.showPopover = this.showPopover.bind(this);
+    this.hidePopover = this.hidePopover.bind(this);
+  }
+
+  showPopover(element) {
+    const popoverElement = document.createElement('div');
+    popoverElement.classList.add('popover');
+    const popoverTitle = document.createElement('div');
+    popoverTitle.classList.add('popoverTitle');
+    popoverTitle.textContent = 'Popover title';
+    const popoverContent = document.createElement('div');
+    popoverContent.classList.add('popoverContent');
+    popoverContent.textContent = "You've clicked on this beautiful button, isn't that amazing?";
+    popoverElement.appendChild(popoverTitle);
+    popoverTitle.insertAdjacentElement('afterend', popoverContent);
+
+    const id = performance.now();
+
+    this.popovers.push({
+      id,
+      element: popoverElement,
+    });
+
+    document.body.appendChild(popoverElement);
+    const { left, top } = element.getBoundingClientRect();
+
+    popoverElement.style.left = `${left + element.offsetWidth / 2 - popoverElement.offsetWidth / 2}px`;
+    popoverElement.style.top = `${top - popoverElement.offsetHeight - 5}px`;
+
+    return id;
+  }
+
+  hidePopover(id) {
+    const popover = this.popovers.find((t) => t.id === id);
+
+    popover.element.remove();
+
+    this.popovers = this.popovers.filter((t) => t.id !== id);
+  }
+}
